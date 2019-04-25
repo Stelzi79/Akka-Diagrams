6ef5f09b6e997e544e7e77972e97d4b65ddb5fbd
Stelzi79 <969932+Stelzi79@users.noreply.github.com>
2019-04-25 22:02:01 +0200

    added console app for dotnet global tool

---
+++ src/dotnet-akkadiagram/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace dotnet_akkadiagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
